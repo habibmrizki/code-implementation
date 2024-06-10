fe87cd12ed01203b6457d8a9ceb5a47b743eecf8
Habib Muhammad Rizki <111440301+habibmrizki@users.noreply.github.com>
2024-06-10 16:19:54 +0700

    Add files via upload

--- task2.js
+++ task2.js
@@ -1,7 +1,24 @@
-const reverseWords = (sentence) => {
-  const words = sentence.split(" ");
-  const revWords = words.reverse();
-  return revWords.join(" ");
+const reverseWords = (kalimat) => {
+  let hasil = "";
+  let ba = 0;
+  let bb = 0;
+
+  for (let i = 0; i <= kalimat.length; i++) {
+    if (kalimat[i] === " " || i === kalimat.length) {
+      for (let j = bb - 1; j >= ba; j--) {
+        hasil = kalimat[j] + hasil;
+      }
+
+      if (i === kalimat.length) {
+        hasil = hasil;
+      } else {
+        hasil = " " + hasil;
+      }
+      ba = i + 1;
+    }
+    bb++;
+  }
+  return hasil;
 };
 
-console.log(reverseWords("Saya belajar Javascript"));
+console.log(reverseWords("Saya Belajar Javascript"));
